ba5ceef2e3313ca7653d12b49dcc64257aaed0b1
rajkumar.sharma <rajkumar.sharma@srijans-MacBook-Pro-2.local>
2018-05-10 12:55:05 +0530

    Pod setup and downloaded required dependancies. Added bridging header file.

---
+++ CGODemoApp/CGODemoApp/CGODemo_Bridging_Header.h
@@ -0,0 +1,14 @@
+//
+//  CGODemo_Bridging_Header.h
+//  CGODemoApp
+//
+//  Created by rajkumar.sharma on 5/10/18.
+//  Copyright © 2018 rajkumar.sharma. All rights reserved.
+//
+
+#ifndef CGODemo_Bridging_Header_h
+#define CGODemo_Bridging_Header_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+
+#endif /* CGODemo_Bridging_Header_h */
---
+++ CGODemoApp/Pods/Target Support Files/Pods-CGODemoApp/Pods-CGODemoApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CGODemoApp : NSObject
+@end
+@implementation PodsDummy_Pods_CGODemoApp
+@end
